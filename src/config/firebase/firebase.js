43832ccf76fbba6d892cefe0385308ded310ee48
devnaveedabbasi <naveedabbasi03111309060@gmail.com>
2026-04-28 18:03:33 +0500

    notifcatoin solve

--- src/config/firebase/firebase.js
+++ src/config/firebase/firebase.js
@@ -4,14 +4,21 @@ import admin from "firebase-admin";
 
 const serviceAccount = JSON.parse(
   fs.readFileSync(
-    path.join(process.cwd(), "src/config/firebase/firebase-service-account.json"),
+    path.join(
+      process.cwd(),
+      "src/config/firebase/firebase-service-account.json"
+    ),
     "utf8"
   )
 );
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert({
+      project_id: serviceAccount.project_id,
+      client_email: serviceAccount.client_email,
+      private_key: serviceAccount.private_key.replace(/\\n/g, "\n"),
+    }),
   });
 }
 
