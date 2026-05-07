d6dfdebada7f621811a087a102c2e2e817317a37
devnaveedabbasi <naveedabbasi03111309060@gmail.com>
2026-05-07 13:17:11 +0500

    new update

--- src/queues/jobAutoCancel.js
+++ src/queues/jobAutoCancel.js
@@ -16,8 +16,11 @@ const jobAutoCancelQueue = new Queue('job-auto-cancel', {
  */
 export async function scheduleAutoCancelJob(jobId) {
   try {
-    const eightHoursInMs = 8 * 60 * 60 * 1000;
-    const oneHourInMs = 60 * 60 * 1000;
+// TODO: TESTING VALUES - Change back to 8 hours in production
+// const eightHoursInMs = 8 * 60 * 60 * 1000;
+// const oneHourInMs = 60 * 60 * 1000;
+const eightHoursInMs = 5 * 60 * 1000;   // 5 minutes (was 8 hours)
+const oneHourInMs = 1 * 60 * 1000;      // 1 minute  (was 1 hour)
     
     console.log(`\n${'='.repeat(60)}`);
     console.log(`AUTO-CANCEL SCHEDULING FOR JOB: ${jobId}`);
